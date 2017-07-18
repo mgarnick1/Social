56114aa667093205a5cf09c9d86763034eaa8d01
Shoenick <markgman008@gmail.com>
2017-07-18 14:03:48 -0600

    Sign in ui custom classes complete

---
+++ Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Shoenick on 7/18/17.
+//  Copyright © 2017 Shoenick. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
